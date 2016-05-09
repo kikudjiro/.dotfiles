412e1970f56b0078ebf0edfa61413d3d9891df2e
Alexander Alexeychuk <kikudjiro@gmail.com>
2016-05-09 15:13:35 +0300

    merge

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # dotfiles
 My dotfiles for linux/osx environment.
+To use, clone to ~/.dotfiles, then symlink to specified locations.
+
+
+
--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.screenrc ~/.screenrc
+ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
 
