11b44db0d241c908965d10fac6dc6004bc9be499
Alexander Alexeychuk <kikudjiro@gmail.com>
2016-05-22 19:09:24 +0300

    zsh and tmux

--- install.sh
+++ install.sh
@@ -5,4 +5,6 @@ ln -s ~/.dotfiles/.screenrc ~/.screenrc
 mkdir ~/bin
 ln -s ~/.dotfiles/bin/mcc ~/bin/mcc
 ln -s ~/.dotfiles/.bash_aliases ~/.bash_aliases
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
